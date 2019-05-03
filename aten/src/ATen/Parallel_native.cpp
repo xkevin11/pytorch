9ee9bfb5b91bd0016233ca1b1ff5ca6228fff8d8
ilia-cher <iliacher@fb.com>
2019-05-02 18:38:54 -0700

    Native ATen/Parallel backend
    Summary:
    Implementing ATen/Parallel withtout OpenMP using native thread pool
    
    Test Plan:
    PARALLEL_BACKEND=NATIVE BLAS=MKL USE_MKLDNN=1 USE_OPENCV=1 USE_FFMPEG=1
    python setup.py develop --cmake
    pytest -s -v test/test_torch.py::TestTorch
    
    gh-metadata: pytorch pytorch 20087 gh/ilia-cher/15/head

---
+++ aten/src/ATen/Parallel_native.cpp
@@ -0,0 +1,115 @@
+#if AT_PARALLEL_NATIVE
+#include <ATen/Parallel_native.h>
+#include <ATen/PTThreadPool.h>
+
+#include <atomic>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+#ifdef TH_BLAS_MKL
+#include <mkl.h>
+#endif
+
+namespace at {
+namespace {
+// Number of threads set by the user
+// -1 -> positive value -> -2
+// or
+// -1 -> -2
+// Meaning:
+//  - -1 - pool not initialized, user value is not set
+//  - positive value - pool not initialized, user value set
+//  - -2 - pool is initialized
+std::atomic<int> num_intraop_threads{-1};
+
+// used with _set_in_parallel_region to mark master thread
+// as in parallel region while executing parallel primitives
+thread_local bool in_parallel_region_ = false;
+
+// thread number (task_id) set by parallel primitive
+thread_local size_t thread_num_ = 0;
+} // namespace
+
+namespace internal {
+
+TaskThreadPoolBase& _get_intraop_pool() {
+  static std::shared_ptr<TaskThreadPoolBase> pool =
+      ThreadPoolRegistry()->Create(
+          "C10",
+          /* device_id */ 0,
+          // minus one because of the master thread
+          /* pool_size */ num_intraop_threads.exchange(-2) - 1,
+          /* create_new */ true); // create a separate thread pool for intra-op
+  return *pool;
+}
+
+void _set_in_parallel_region(bool in_region) {
+  in_parallel_region_ = in_region;
+}
+
+void _set_thread_num(size_t thread_num) {
+  thread_num_ = thread_num;
+}
+
+void _unset_thread_num() {
+  thread_num_ = 0;
+}
+
+} // namespace internal
+
+//TODO: use OMP and MKL env. vars as default values
+void init_num_threads() {
+  #ifdef _OPENMP
+  omp_set_num_threads(1);
+  #endif
+
+  #ifdef TH_BLAS_MKL
+  mkl_set_num_threads(1);
+  #endif
+}
+
+void set_num_threads(int nthreads) {
+  if (nthreads <= 0) {
+    throw std::runtime_error(
+      "Expected positive number of threads");
+  }
+  int no_value = -1;
+  if (!num_intraop_threads.compare_exchange_strong(no_value, nthreads)) {
+    throw std::runtime_error(
+      "Error: cannot set number of interop threads "
+      "after parallel work has started or after set_num_threads call");
+  }
+}
+
+int get_num_threads() {
+  // not initializing pool unnecessarily,
+  // because pool cannot be resized after initialization
+  int nthreads = num_intraop_threads.load();
+  if (nthreads > 0) {
+    return nthreads;
+  } else if (nthreads == -1) {
+    // add plus one because master thread is also used in
+    // parallel computation
+    return TaskThreadPoolBase::defaultNumThreads() + 1;
+  } else {
+    AT_ASSERT(nthreads == -2);
+    return internal::_get_intraop_pool().size() + 1;
+  }
+}
+
+int get_thread_num() {
+  return thread_num_;
+}
+
+bool in_parallel_region() {
+  return in_parallel_region_ || (
+    // pool is already created or single thread case
+    num_intraop_threads.load() == -2 &&
+    internal::_get_intraop_pool().inThreadPool()
+  );
+}
+
+} // namespace at
+#endif
--- aten/src/ATen/Parallel_threadpool_native.cpp
+++ aten/src/ATen/Parallel_threadpool_native.cpp
@@ -1,4 +1,4 @@
-#if AT_PARALLEL_OPENMP
+#if AT_PARALLEL_OPENMP || AT_PARALLEL_NATIVE
 #include <ATen/Parallel.h>
 #include <ATen/PTThreadPool.h>
 
@@ -29,46 +29,56 @@ TaskThreadPoolBase& get_pool() {
   return *pool;
 }
 
-std::shared_ptr<TaskThreadPoolBase> createC10ThreadPool(
-    int device_id,
-    int pool_size,
-    bool create_new) {
+std::shared_ptr<TaskThreadPoolBase> get_shared_threadpool(int pool_size) {
   static std::shared_ptr<TaskThreadPoolBase> pool =
-      std::make_shared<PTThreadPool>(
-          pool_size > 0 ? pool_size : std::thread::hardware_concurrency());
-  // For now, the only accepted device id is 0
-  // for the JIT inter-op pool (CPU),
-  AT_ASSERT(device_id == 0);
-  // we use the shared thread pool
-  AT_ASSERT(!create_new);
-  // and the size does not change
+      std::make_shared<PTThreadPool>(pool_size);
+  // the size does not change
   AT_ASSERT(pool->size() == pool_size);
   return pool;
 }
 
+// Factory method for ThreadPoolRegistry
+std::shared_ptr<TaskThreadPoolBase> create_c10_threadpool(
+    int device_id,
+    int pool_size,
+    bool create_new) {
+  // For now, the only accepted device id is 0
+  // for the JIT inter-op pool (CPU),
+  AT_ASSERT(device_id == 0);
+  if (!create_new) {
+    // use existing shared thread pool
+    return get_shared_threadpool(pool_size);
+  } else {
+    // create a new thread pool
+    return std::make_shared<PTThreadPool>(pool_size);
+  }
+}
+
 } // namespace
 
-C10_REGISTER_CREATOR(ThreadPoolRegistry, C10, createC10ThreadPool);
+C10_REGISTER_CREATOR(ThreadPoolRegistry, C10, create_c10_threadpool);
 
-void set_num_interop_threads(size_t nthreads) {
-  if (nthreads == 0) {
-    return;
+void set_num_interop_threads(int nthreads) {
+  if (nthreads <= 0) {
+    throw std::runtime_error(
+      "Expected positive number of threads");
   }
 
   int no_value = -1;
   if (!num_interop_threads.compare_exchange_strong(no_value, nthreads)) {
     throw std::runtime_error(
       "Error: cannot set number of interop threads "
-      "after parallel work has started");
+      "after parallel work has started or after set_num_interop_threads call");
   }
 }
 
-size_t get_num_interop_threads() {
+int get_num_interop_threads() {
   int nthreads = num_interop_threads.load();
   if (nthreads > 0) {
     return nthreads;
   } else if (nthreads == -1) {
-    return std::thread::hardware_concurrency();
+    // return default value
+    return TaskThreadPoolBase::defaultNumThreads();
   } else {
     return get_pool().size();
   }
